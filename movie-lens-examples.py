d1518b675db236d6318d9b4626dac84d34e553aa
vanalex <apjorquera@gmail.com>
2018-01-30 19:31:34 +0100

    add some more examples of selecting

--- movie-lens-examples.py
+++ movie-lens-examples.py
@@ -32,3 +32,15 @@ print('')
 columns_you_want = ['occupation', 'sex']
 print(users[columns_you_want].head())
 
+# Row selection can be done multiple ways, but doing so by an individual index or boolean indexing are typically easiest
+# users older than 25
+print('')
+print(users[users.age > 25].head(3))
+
+# users aged 40 AND male
+print('')
+print(users[(users.age == 40) & (users.sex == 'M')].head(3))
+
+# users younger than 30 OR female
+print('')
+print(users[(users.sex == 'F') | (users.age < 30)].head(3))
